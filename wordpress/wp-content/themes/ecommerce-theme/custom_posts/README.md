749cb61fac639bd9fca871d04943e12ad5284a23
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2023-01-15 08:27:19 -0800

    finished making basic layout

--- wordpress/wp-content/themes/ecommerce-theme/custom_posts/README.md
+++ wordpress/wp-content/themes/ecommerce-theme/custom_posts/README.md
@@ -1,5 +1,9 @@
 # CUSTOM POSTS
 
-### What is custom posts
+### What is custom posts:
+
+- Custom post are the content
+
+### Why we are using this:
 
 - This is to separate the files as much as possible and install custom posts
--- wordpress/wp-content/themes/ecommerce-theme/index.php
+++ wordpress/wp-content/themes/ecommerce-theme/index.php
@@ -1,10 +1,5 @@
 <?php get_header(); ?>
-</head>
 
-<body class="container">
-  <div class="row">
-    <section class="col-4">Test 1</section>
-    <section class="col-4">Test 2</section>
-    <section class="col-4">Test 3</section>
-  </div>
-</body>
+
+
+<?php get_footer(); ?>
