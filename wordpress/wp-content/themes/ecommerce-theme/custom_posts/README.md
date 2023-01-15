2a10ea07193856e27c06c27208015a1d0a6d73b6
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2023-01-14 18:42:06 -0800

    Creating e-commerce theme

---
+++ wordpress/wp-content/themes/ecommerce-theme/custom_posts/README.md
@@ -0,0 +1,5 @@
+# CUSTOM POSTS
+
+### What is custom posts
+
+- This is to separate the files as much as possible and install custom posts
---
+++ wordpress/wp-content/themes/ecommerce-theme/index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+</head>
+
+<body class="container">
+  <div class="row">
+    <section class="col-4">Test 1</section>
+    <section class="col-4">Test 2</section>
+    <section class="col-4">Test 3</section>
+  </div>
+</body>
