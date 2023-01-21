956225275fd55853fd381a67da041351d70adbbc
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2023-01-20 21:01:53 -0800

    added footer and functions

---
+++ wordpress/wp-content/themes/ecommerce-theme/function.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Enqueue the scripts can do js */
+
+// function startwordpress_scripts() {
+// 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
+// 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
+// }
+
+// add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
--- wordpress/wp-content/themes/fictional-university-theme/functions.php
+++ wordpress/wp-content/themes/fictional-university-theme/functions.php
@@ -3,7 +3,6 @@
 /* This is a hook */
 function startwordpress_scripts() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
-	wp_enqueue_style( 'blog', get_template_directory_uri() . '/assets/css/blog.css' );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
 }
 
